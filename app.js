3b2eaeee300e7032dab5649bf9399694ad76af32
adamsmoha <adamsmoha@2019.com>
2023-12-23 00:19:45 +0000

    Added Body Parser to get form data

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express= require('express');
 const https = require('https');
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
-    const url ='https://api.openweathermap.org/data/2.5/weather?appid=b5444fefae497fc69c523c0393dcadf0&q=Ghana&units=metric';
+    res.sendFile(__dirname+'/index.htm');
+});
+
+app.post('/',(req,res)=>{
+    const query=req.body.cityName;
+    const apiKey='b5444fefae497fc69c523c0393dcadf0';
+    const units='metric';
+    const url =`https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${query}&units=${units}`;
     https.get(url,(response)=>{
         console.log(response.statusCode);
         response.on('data',(data)=>{
@@ -14,12 +23,11 @@ app.get('/',(req,res)=>{
             const icon = weatherData.weather[0].icon;
             const image = `https://openweathermap.org/img/wn/${icon}@2x.png`;
             res.write(`<p>The weather is currently ${desc} </p>`);
-            res.write(`<h1>The temperature in Ghane is ${temp} degrees Celcius.</h1>`);
+            res.write(`<h1>The temperature in ${query} is ${temp} degrees Celcius.</h1>`);
             res.write(`<img src='${image}'/>`);
             res.send();
         })
     })
-});
-
+})
 
 app.listen(3000,()=>console.log('Server started at Port 3000'));
